8dae8970ac85b9cc61d17072fa41aab69f5aa179
May Lau <ascriptmaster@gmail.com>
2024-06-11 16:38:44 -0700

    Release 1.0.0

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nihility
+This mod adds the challenge "Nihility" to Balatro, in which all cards are debuffed. To make Ante 1 winnable, you start with $8 and a single Joker. Good luck!
+
+## Installation
+This mod uses the Steamodded loader. Installation instructions for the loader can be found [here](https://github.com/Steamopollys/Steamodded/wiki/01.-Getting-started). The Lua file can be placed directly in the `Mods` directory; the surrounding directory is not necessary.
+
+It currently only works on Steamodded 0.9.8. Support for Steamodded 1.0.0 is underway.
---
+++ nihility.lua
@@ -0,0 +1,110 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Nihility Challenge
+--- MOD_ID: NihilityChallenge
+--- MOD_AUTHOR: [ascriptmaster]
+--- MOD_DESCRIPTION: A mod that adds the Nihility Challenge Deck to the game Balatro, in which all cards are debuffed.
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+function SMODS.INIT.NihilityChallenge()
+    -- Challenge definition
+    local challenges = G.CHALLENGES
+    table.insert(G.CHALLENGES,1,{
+        name = 'Nihility',
+        id = 'c_mod_nihility',
+        rules = {
+            custom = {
+                {id = 'debuff_all'}
+            },
+            modifiers = {
+                {id = 'dollars', value = 8},
+            }
+        },
+        jokers = {
+            {id = 'j_joker'}
+        },
+        consumeables = {},
+        vouchers = {
+        },
+        deck = {
+            type = 'Challenge Deck',
+        },
+        restrictions = {
+            banned_cards = {
+                {id = 'j_8_ball'},
+                {id = 'j_ancient'},
+                {id = 'j_arrowhead'},
+                {id = 'j_baron'},
+                {id = 'j_bloodstone'},
+                {id = 'j_business'},
+                {id = 'j_castle'},
+                {id = 'j_dusk'},
+                {id = 'j_even_steven'},
+                {id = 'j_faceless'},
+                {id = 'j_fibonacci'},
+                {id = 'j_gluttenous_joker'},
+                {id = 'j_greedy_joker'},
+                {id = 'j_hack'},
+                {id = 'j_hanging_chad'},
+                {id = 'j_hiker'},
+                {id = 'j_hit_the_road'},
+                {id = 'j_idol'},
+                {id = 'j_lusty_joker'},
+                {id = 'j_mail'},
+                {id = 'j_midas_mask'},
+                {id = 'j_mime'},
+                {id = 'j_odd_todd'},
+                {id = 'j_onyx_agate'},
+                {id = 'j_pareidolia'},
+                {id = 'j_photograph'},
+                {id = 'j_raised_fist'},
+                {id = 'j_reserved_parking'},
+                {id = 'j_rough_gem'},
+                {id = 'j_scary_face'},
+                {id = 'j_scholar'},
+                {id = 'j_seeing_double'},
+                {id = 'j_selzer'},
+                {id = 'j_shoot_the_moon'},
+                {id = 'j_smiley'},
+                {id = 'j_sock_and_buskin'},
+                {id = 'j_splash'},
+                {id = 'j_ticket'},
+                {id = 'j_triboulet'},
+                {id = 'j_vampire'},
+                {id = 'j_walkie_talkie'},
+                {id = 'j_wee'},
+                {id = 'j_wrathful_joker'},
+            },
+            banned_tags = {
+            },
+            banned_other = {
+                {id = 'bl_club', type = 'blind'},
+                {id = 'bl_goad', type = 'blind'},
+                {id = 'bl_head', type = 'blind'},
+                {id = 'bl_plant', type = 'blind'},
+                {id = 'bl_window', type = 'blind'},
+                {id = 'bl_pillar', type = 'blind'},
+                {id = 'bl_final_leaf', type = 'blind'},
+            }
+        }
+    })
+
+    -- Localization
+    G.localization.misc.challenge_names.c_mod_nihility = "Nihility"
+    G.localization.misc.v_text.ch_c_debuff_all = {"ALL cards are debuffed"}
+    init_localization()
+end
+
+-- Inject debuff enforcement
+local Blind_debuff_card_ref = Blind.debuff_card
+function Blind:debuff_card(card, from_blind)
+    if G.GAME.challenge == "c_mod_nihility" and card and card.area ~= G.jokers then
+        card:set_debuff(true)
+        return
+    end
+    return Blind_debuff_card_ref(self, card, from_blind)
+end
+
+----------------------------------------------
+------------MOD CODE END----------------------
